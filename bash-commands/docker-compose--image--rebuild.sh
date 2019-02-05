641a2023850039edd09cad265a380e72b4fcc65f
David Lechner <david.lechner@talsen.team>
2019-02-05 10:34:26 +0100

    fix echo outputs

--- bash-commands/docker-compose--image--rebuild.sh
+++ bash-commands/docker-compose--image--rebuild.sh
@@ -6,9 +6,9 @@ source bash-util/functions.sh
 
 prepare_local_environment ${@}
 
-echo -E "Building docker images ..."
+echo -E "Rebuilding docker images ..."
 
 docker-compose --file ${HOST_PATH_TO_DOCKER_COMPOSE_FILE} \
                build  --no-cache
 
-echo -e "Building docker images ... $( __done )"
+echo -e "Rebuilding docker images ... $( __done )"
